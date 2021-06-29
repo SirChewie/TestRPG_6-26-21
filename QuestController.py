2de285da739c99c1878f55ed063190a2acfdc291
EyeceScream <jhb1085@gmail.com>
2021-06-29 11:40:24 -0500

    Update QuestController.py

--- QuestController.py
+++ QuestController.py
@@ -2,6 +2,22 @@ class Quests():
     def __init__(self):
         pass
 
-    name = ''
-    levelReq = ''
-    xpGain = 10
+    #Current quest player is on
+    currQuest = ''
+    #Current Quest Objective
+    qCurrObjective = ''
+    #Completed Quest Objective
+    qCompObjective = ''
+    qFinished = ''
+
+    finishedQuests = []
+    def introduction(self):
+        name = 'Introduction'
+        levelReq = 0
+        xpGain = 10
+
+def is_finished(self):
+    if (str(self.qCurrObjective) == str(self.qCompObjective)):
+        self.qFinished = True
+    else:
+        self.qFinished = False
