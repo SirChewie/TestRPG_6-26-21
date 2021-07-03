61cbb1e15ec8871a898c9f0ce14a8729c412fd07
EyeceScream <jhb1085@gmail.com>
2021-07-02 23:03:55 -0500

    enemy changes.

--- EnemyController.py
+++ EnemyController.py
@@ -1,60 +1,25 @@
-import LootController
-
 class Enemy:
-    def __init__(self):
-        pass
-    name = ''
-    level = 1
-    pCurrXP = 0
-    pMaxXP = 100
-    hp = 0
-    playerDmg = 0
-    s_str = 2
-    s_vit = 10
-    #Equiped by Enemy
-    class EquipedStats:
-        class Helmet:
-            name = ''
-            level = 0
-            pRes = 0
-            mRes = 0
-        class Armor:
-            name = ''
-            level = 0
-            pRes = 0
-            mRes = 0
-        class Legs:
-            name = ''
-            level = 0
-            pRes = 0
-            mRes = 0
-        class Weapon:
-            name = ''
-            level = 0
-            dmg = 1
-    #Carried by Enemy
-    class Inventory:
-        coins = 0
-        items_I=[]
-        #sets up equiped equipment
-    def setup_epuiped(self):
-        #setup to test system
-        Enemy.Inventory.items_I.append(LootController.Loot.TestWep.name)
-        Enemy.Inventory.items_I.append(LootController.Loot.TestWep.name)
-        Enemy.Inventory.items_I.append(LootController.Loot.TestWep.name)
 
-    #Calculates dmg for Enemy
-    def player_dmg_calc(self):
-        self.playerDmg = (self.EquipedStats.Weapon.dmg + self.s_str)
-
-    #prints Enemy's stats
-    def print_stats(self):
-        print(
-            "Player Stats: " +
-            "\n Name: " + str(Enemy.name) +
-            "\n Level: " + str(Enemy.level) +
-            "\n XP : " + str(Enemy.pCurrXP) + "/" + str(Enemy.pMaxXP) +
-            "\n Health: " + str(Enemy.hp) +
-            "\n Strength: " + str(Enemy.s_str) +
-            "\n Vitality: " + str(Enemy.s_vit)
-              )
+    def __init__(self,
+                 name,
+                 level,
+                 pCurrXP,
+                 pMaxXP,
+                 hp,
+                 enemy_dmg,
+                 s_str,
+                 s_vit):
+        self.name = name
+        self.level = level
+        self.pCurrXP = pCurrXP
+        self.pMaxXP = pMaxXP
+        self.hp = hp
+        self.enemy_dmg = enemy_dmg
+        self.s_str = s_str
+        self.s_vit = s_vit
+    currQuest = ''
+    qCurrObjective = ''
+    qCompObjective = ''
+    inventory = []
+    equipped = []
+    coins = 0
